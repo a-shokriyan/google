f05be4da7b9209628a67c345db911c901d22651b
a-shokriyan <shokriyan@gmail.com>
2018-12-29 11:55:48 -0500

    First add

---
+++ src/test/java/com/tests/OpenBrowser.java
@@ -0,0 +1,21 @@
+package com.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+	
+	@Test
+	public void browser () {
+		
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("http://www.google.com");
+		
+		driver.close();
+		
+	}
+
+}
